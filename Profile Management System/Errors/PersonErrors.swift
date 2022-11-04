5cc170273c2d4221e4d2b7f5d7cdff0fb8975d56
sanjeev-rm <Sanjeevraghu2050@gmail.com>
2022-11-04 10:05:52 +0530

    Added / Edited comments.

--- Profile Management System/Errors/PersonErrors.swift
+++ Profile Management System/Errors/PersonErrors.swift
@@ -7,23 +7,35 @@
 
 import Foundation
 
+/// Contains errors of a Date.
 enum DateError : Error
 {
+    /// Error for when the year is invalid.
     case invalidYearError
+    /// Error for when the Month is invalid.
     case invalidMonthError
+    /// Error for when the Day is invalid.
     case invalidDayError
+    /// Error for when an String that's entered can't be converted to an Int value.
     case typeMismatchError
 }
 
+/// Contains erros of a Email.
 enum EmailError : Error
 {
+    /// Error for when the email is invalid.
     case invalidEmailError
+    /// Error for when the recipient name of the email is invalid.
     case invalidRecipientNameError
+    /// Error for when the domain name of the email is inavlid.
     case invalidDomainNameError
 }
 
+/// Contains errors of a mobile number.
 enum MobileError : Error
 {
+    /// Error for when the length of the mobile number is invalid.
     case lengthInvalidError
+    /// Error for when an String that's entered can't be converted to an Int value.
     case typeMismatchError
 }
--- Profile Management System/Errors/StudentErrors.swift
+++ Profile Management System/Errors/StudentErrors.swift
@@ -7,8 +7,11 @@
 
 import Foundation
 
+/// Contains errors of Graduating Year.
 enum GraduatingYearError : Error
 {
+    /// Error for when year is invalid.
     case invalidYearError
+    /// Error for when an String that's entered can't be converted to an Int value.
     case typeMismatchError
 }
